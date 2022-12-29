494d02bb3f766569240daefcdce4f6bdde7f33bf
Tran Thang <tranthang.dev@gmail.com>
2022-12-29 20:27:03 +0700

    create convertMillisecondsToHourMinute function

--- Frontend/src/utils/DateTimeUtils.ts
+++ Frontend/src/utils/DateTimeUtils.ts
@@ -11,3 +11,18 @@ export function convertDateToAmPm(date: Date): string {
     minute: "2-digit",
   });
 }
+
+export function convertMillisecondsToHourMinute(milliseconds: number): string {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  seconds = seconds % 60;
+
+  minutes = seconds >= 30 ? minutes + 1 : minutes;
+  minutes = minutes % 60;
+
+  hours = hours % 24;
+
+  return `${padTo2Digits(hours)}h ${padTo2Digits(minutes)}m`;
+}
