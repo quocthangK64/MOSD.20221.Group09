2d72851637a37a575b61780297e1b17580b187f2
VinhTongThanh <vinhtongthanh57@gmail.com>
2022-12-30 08:20:26 +0700

    create PassengerService class

---
+++ Backend/src/main/java/com/ict/group09/travelwala/passenger/service/PassengerService.java
@@ -0,0 +1,8 @@
+package com.ict.group09.travelwala.passenger.service;
+
+import com.ict.group09.travelwala.model.request.PassengerRequest;
+import com.ict.group09.travelwala.model.response.PassengerResponse;
+
+public interface PassengerService {
+    // TODO
+}
