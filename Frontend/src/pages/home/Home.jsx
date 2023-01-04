f645a4f9cb435458f2c2973fc46990171cf42fa0
Quan Minh <quanminh2019@gmail.com>
2023-01-04 20:19:43 +0700

    Update style for Home.js

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,15 +1,37 @@
-// import FlightSearchBox from "./components/FlightSearchBox";
-import SwitchSearch from './components/SwitchSearch';
-import backgroundImage from '../../assets/sunset-beach.jpg';
+vvimport SwitchSearch from "./components/SwitchSearch";
+import backgroundImage from "../../assets/sunset-beach.jpg";
+import { Box, Typography } from "@mui/material";
+import { NewsLetter } from "../../components/NewsLetter";
 
 function Home() {
   return (
-    <div style={{position: "static", height: "2000px"}}>
-      {/*<FlightSearchBox/>*/}
-      <img src={backgroundImage} style={{width: '100%'}} alt="background-home"/>
-      <SwitchSearch id="search_box"/>
-    </div>
-  )
+    <Box
+      style={{
+        position: "static",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <img
+        src={backgroundImage}
+        style={{ width: "100%" }}
+        alt="background-home"
+      />
+      <Typography
+        fontSize="60px"
+        fontWeight={400}
+        textAlign="center"
+        color="#3B505A"
+        margin="40px 0px 40px"
+      >
+        BOOK YOUR TRAVEL
+      </Typography>
+      <SwitchSearch id="search_box" />
+      <Box style={{ marginTop: "193px" }}>
+        <NewsLetter />
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;
