397b024f24886191497b338b37b8a4e6e1eef6bf
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-01-04 09:55:19 +0700

    Create PassengerServiceImpl.java

---
+++ Backend/src/main/java/com/ict/group09/travelwala/passenger/service/impl/PassengerServiceImpl.java
@@ -0,0 +1,17 @@
+package com.ict.group09.travelwala.passenger.service.impl;
+
+import com.ict.group09.travelwala.mapstruct.mappers.PassengerMapper;
+import com.ict.group09.travelwala.model.request.PassengerRequest;
+import com.ict.group09.travelwala.model.response.PassengerResponse;
+import com.ict.group09.travelwala.passenger.entity.Passenger;
+import com.ict.group09.travelwala.passenger.repository.PassengerRepository;
+import com.ict.group09.travelwala.passenger.service.PassengerService;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PassengerServiceImpl implements PassengerService {
+    // TODO
+}
