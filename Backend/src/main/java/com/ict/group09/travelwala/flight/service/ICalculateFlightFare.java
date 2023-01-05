5520ea72e4b61ccb6949b228479f369f9ed09834
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-01-05 22:57:31 +0700

    Create ICalculateFlightFare.java

---
+++ Backend/src/main/java/com/ict/group09/travelwala/flight/service/ICalculateFlightFare.java
@@ -0,0 +1,8 @@
+package com.ict.group09.travelwala.flight.service;
+
+import com.ict.group09.travelwala.common.enumeration.seatclass.SeatClassEnum;
+import com.ict.group09.travelwala.ticket.enumeration.TicketEnum;
+
+public interface ICalculateFlightFare {
+    // TODO
+}
