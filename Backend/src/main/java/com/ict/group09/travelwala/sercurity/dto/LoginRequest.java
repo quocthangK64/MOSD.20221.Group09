3662905e04b7dc64f50f100b33109d39f67063d7
phananh1310 <108656878+Sotatek-AnhPhan2@users.noreply.github.com>
2023-01-05 19:50:10 +0700

    create dtos for user login, register
    include oauth2, username password

---
+++ Backend/src/main/java/com/ict/group09/travelwala/sercurity/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.ict.group09.travelwala.sercurity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    String username;
+    String password;
+}
