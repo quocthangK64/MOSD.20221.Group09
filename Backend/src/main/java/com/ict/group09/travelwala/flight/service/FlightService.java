6e205c5fd6ac9a97497738495cb5744b808782c3
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-01-05 22:55:31 +0700

    Create FlightService.java

---
+++ Backend/src/main/java/com/ict/group09/travelwala/flight/service/FlightService.java
@@ -0,0 +1,10 @@
+package com.ict.group09.travelwala.flight.service;
+
+import com.ict.group09.travelwala.flight.model.request.FlightCriteria;
+import com.ict.group09.travelwala.model.response.FlightResponse;
+import com.ict.group09.travelwala.flight.model.request.FlightRequest;
+import com.ict.group09.travelwala.flight.model.response.SearchFlightResponse;
+
+public interface FlightService {
+    // TODO
+}
