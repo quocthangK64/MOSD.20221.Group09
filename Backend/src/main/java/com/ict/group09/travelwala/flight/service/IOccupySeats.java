e0af0e55e94072d5f67c3415960c8aa31d1000e8
VinhTongThanh <vinhtongthanh57@gmail.com>
2023-01-05 22:56:40 +0700

    Update IOccupySeats.java

--- Backend/src/main/java/com/ict/group09/travelwala/flight/service/IOccupySeats.java
+++ Backend/src/main/java/com/ict/group09/travelwala/flight/service/IOccupySeats.java
@@ -3,5 +3,5 @@ package com.ict.group09.travelwala.flight.service;
 import com.ict.group09.travelwala.common.enumeration.seatclass.SeatClassEnum;
 
 public interface IOccupySeats {
-    // TODO
+    void occupy(String flightId, int numberOfSeats, SeatClassEnum seatClass);
 }
