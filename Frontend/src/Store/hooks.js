6b3bef309a71797eed543154e64155099f79caa3
KimTrung781 <nkttrung78@gmail.com>
2023-01-12 21:06:04 +0700

    Update folder store

--- Frontend/src/Store/hooks.js
+++
@@ -1,9 +0,0 @@
-import Context from "./Context";
-import {useContext} from "react";
-
-function useStore() {
-    const [state, dispatch] = useContext(Context)
-    return [state, dispatch];
-}
-
-export {useStore};
