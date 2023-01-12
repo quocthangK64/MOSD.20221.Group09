43775b30be362cae2e4d2d0e6a93df35d56014a4
Tran Thang <tranthang.dev@gmail.com>
2023-01-12 23:33:07 +0700

    fixed import path

--- Frontend/src/api/flight/FlightService.ts
+++ Frontend/src/api/flight/FlightService.ts
@@ -1,4 +1,4 @@
-import { FlightCriteria, SearchFlightResponse } from "../FlightService.types";
+import { FlightCriteria, SearchFlightResponse } from "./FlightService.types";
 import axios, { AxiosResponse } from "axios";
 import { axiosConfig } from "../config";
 
