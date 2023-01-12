96f6ff882dcba2016d027892b4a54b85a431b54e
Tran Thang <tranthang.dev@gmail.com>
2023-01-12 23:32:38 +0700

    update FlightService class api

--- Frontend/src/api/flight/FlightService.ts
+++ Frontend/src/api/flight/FlightService.ts
@@ -1,3 +1,13 @@
 import { FlightCriteria, SearchFlightResponse } from "../FlightService.types";
 import axios, { AxiosResponse } from "axios";
 import { axiosConfig } from "../config";
+
+class FlightService {
+    static async search(
+      flightCriteria: FlightCriteria
+    ): Promise<AxiosResponse<SearchFlightResponse>> {
+      return axios.post(`/flights/search`, flightCriteria, {
+        ...axiosConfig(),
+      });
+    }
+}
