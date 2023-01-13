a3cddf9e34964cdff2fef422078b659f10eaabc8
Tran Thang <tranthang.dev@gmail.com>
2023-01-13 10:53:15 +0700

    updaete type for flight service

--- Frontend/src/api/flight/FlightService.types.ts
+++ Frontend/src/api/flight/FlightService.types.ts
@@ -13,3 +13,44 @@ export type FlightCriteria = {
     departureFlights?: FlightResponse[];
     returnFlights?: FlightResponse[];
   };
+  
+  export type FlightResponse = {
+    id: string;
+    code: string;
+    adultEconomicPrice: number;
+    adultBusinessPrice: number;
+    discountRate?: number | null;
+    departureCity: string;
+    arrivalCity: string;
+    departureAirport: string;
+    arrivalAirport: string;
+    departureTime: string;
+    expectedArrivalTime: string;
+    occupiedEconomicSeats: number;
+    occupiedBusinessSeats: number;
+    plane: PlaneResponse;
+    airline: AirlineResponse;
+  };
+  
+  export type AirlineResponse = {
+    id: string;
+    name: string;
+    alias?: string | null;
+    iata: string | null;
+    icao: string | null;
+    callSign: string;
+    country: string;
+    active: boolean;
+    childPriceRate: number;
+    imageLink?: string | null;
+  };
+  
+  export type PlaneResponse = {
+    id: string;
+    manufacturer: string;
+    code: string;
+    name: string;
+    maximumEconomicCapacity: number;
+    maximumBusinessCapacity: number;
+  };
+  
