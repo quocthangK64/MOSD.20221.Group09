f6f739f8dbcacba8c1dce5b5c0c2ca242c16ea5d
KimTrung781 <nkttrung78@gmail.com>
2023-02-01 20:29:28 +0700

    Update BookingService api

--- Frontend/src/api/booking/BookingService.types.ts
+++ Frontend/src/api/booking/BookingService.types.ts
@@ -7,3 +7,25 @@ interface CreateBookingFlightSpecs {
     travellerSpecs: TravellerSpecs
     flightProductSpecs: FlightProductSpecs
 }
+interface FlightProductSpecs {
+  departureFlightId: string
+  returnFlightId: string
+  seatClass: string
+  adultCount: number
+  childCount: number
+  infantCount: number
+}
+
+interface TravellerSpecs {
+  adultFormData: TravelerDataForm[]
+  childFormData: TravelerDataForm[]
+  infantFormData: TravelerDataForm[]
+}
+
+interface TravelerDataForm {
+  title: string
+  firstName: string
+  lastName: string
+  dob: DateOfBirth
+  nationality: string
+}
