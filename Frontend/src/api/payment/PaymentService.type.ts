afd18f1d3685627d130286863525c14fc1b24ebf
Tran Thang <tranthang.dev@gmail.com>
2023-02-05 23:58:40 +0700

    add createpayment response interface

--- Frontend/src/api/payment/PaymentService.type.ts
+++ Frontend/src/api/payment/PaymentService.type.ts
@@ -2,3 +2,10 @@ export interface PaymentRequest {
     amount: number;
     currency: string;
   }
+
+export interface CreatePaymentResponse {
+    amount: number;
+    currency: string;
+    clientSecret: string;
+  }
+  
